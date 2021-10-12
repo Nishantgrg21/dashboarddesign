f2a1682292d85cd666a5953cb04daa170e7537d9
nishantgrg21 <nishantgrg21@gmail.com>
2021-10-12 21:59:04 +0530

    new dashboard changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout/layout.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+   
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    LayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: []
+})
+export class AppModule { }
